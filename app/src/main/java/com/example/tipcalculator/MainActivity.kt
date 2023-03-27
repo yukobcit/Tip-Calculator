7a22df5b60afbe90ae3b210d944ec154b272e1cd
Yuko Ishida <yishida2@my.bcit.ca>
2023-03-27 13:56:06 -0700

    tip calculator version3

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -19,8 +22,12 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.DecimalFormat
 import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
@@ -43,13 +50,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipCalculatorScreen() {
     var serviceCostAmountInput by remember { mutableStateOf("") }
+    var tipAmount by remember { mutableStateOf(0.0) }
     val amount = serviceCostAmountInput.toDoubleOrNull() ?: 0.0
-    var tipAmount by remember { mutableStateOf("") }
     val randomBill = GenerateRandomBill()
+    var totalAmount by remember { mutableStateOf(0.0) }
+    val decimalFormat = DecimalFormat("#.##")
 
     Column(
-        modifier = Modifier.padding(32.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = Modifier
+            .padding(32.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = stringResource(R.string.tip_calculator_heading),
@@ -68,33 +79,98 @@ fun TipCalculatorScreen() {
             onValueChange = { serviceCostAmountInput = it }
         )
 
-        Spacer(Modifier.height(24.dp))
-        Row{
-            Button(onClick = { tipAmount = calculateTip(10.0, amount) }, modifier = Modifier.padding(end = 8.dp)) {
-                Text(text = stringResource(R.string.tip_percent_1))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 32.dp),
+
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth().background(Color.White)
+            ) {
+                Button(
+                    onClick = {
+                        tipAmount = calculateTip(10.0, amount)
+                        totalAmount = decimalFormat.format(amount + tipAmount).toDouble()
+                    },
+                    modifier = Modifier.padding(end = 8.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White, contentColor = Color.Black),
+                    border = BorderStroke(1.dp, Color.Black)
+                ) {
+                    Text(text = stringResource(R.string.tip_percent_1))
+                }
+                Button(
+                    onClick = {
+                        tipAmount = calculateTip(15.0, amount)
+                        totalAmount = decimalFormat.format(amount + tipAmount).toDouble()
+                    },
+                    modifier = Modifier.padding(start = 8.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White, contentColor = Color.Black),
+                    border = BorderStroke(1.dp, Color.Black)
+                ) {
+                    Text(text = stringResource(R.string.tip_percent_2))
+                }
             }
-            Button(onClick = { tipAmount = calculateTip(15.0, amount) }, modifier = Modifier.padding(start = 8.dp)) {
-                Text(text = stringResource(R.string.tip_percent_2))
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {
+                        tipAmount = calculateTip(18.0, amount)
+                        totalAmount = decimalFormat.format(amount + tipAmount).toDouble()
+                    },
+                    modifier = Modifier.padding(end = 8.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White, contentColor = Color.Black),
+                    border = BorderStroke(1.dp, Color.Black)
+                ) {
+                    Text(text = stringResource(R.string.tip_percent_3))
+                }
+                Button(
+                    onClick = {
+                        tipAmount = calculateTip(20.0, amount)
+                        totalAmount = decimalFormat.format(amount + tipAmount).toDouble()
+                    },
+                    modifier = Modifier.padding(start = 8.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White, contentColor = Color.Black),
+                    border = BorderStroke(1.dp, Color.Black)
+                ) {
+                    Text(text = stringResource(R.string.tip_percent_4))
+                }
             }
+
         }
-        Row {
-            Button(onClick = { tipAmount = calculateTip(18.0, amount) }, modifier = Modifier.padding(end = 8.dp)) {
-                Text(text = stringResource(R.string.tip_percent_3))
-            }
-            Button(onClick = { tipAmount = calculateTip(20.0, amount) }, modifier = Modifier.padding(start = 8.dp)) {
-                Text(text = stringResource(R.string.tip_percent_4))
-            }
+
+        Column(
+            modifier = Modifier
+                .padding(32.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            Text(
+                text = stringResource(R.string.bill_amount, "$$amount"),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.tip_amount, "$$tipAmount"),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Divider(color = Color.LightGray, thickness = 4.dp, modifier = Modifier.fillMaxWidth())
+            Text(
+                text = stringResource(R.string.total_amount, "$$totalAmount"),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
         }
-
-
-
-
-        Text(
-            text = stringResource(R.string.tip_amount, tipAmount),
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
-        )
 
     }
 }
@@ -116,20 +192,21 @@ fun EditServiceCostField(
     )
 }
 
-private fun calculateTip(tipPercent: Double,
-    amount: Double
-): String {
+private fun calculateTip(tipPercent: Double, amount: Double, decimalPlaces: Int = 2): Double {
     val tip = tipPercent / 100 * amount
-    return NumberFormat.getCurrencyInstance().format(tip)
+    return BigDecimal(tip).setScale(decimalPlaces, RoundingMode.HALF_UP).toDouble()
 }
 
 
-private fun GenerateRandomBill(): Int {
-    val random = (1..100).random()
-    return random
+private fun GenerateRandomBill(): String {
+    val min = 1.0
+    val max = 1000.0
+    val amount = min + (max - min) * Math.random()
+    val decimalFormat = DecimalFormat("#.##")
+    val formattedAmount = decimalFormat.format(amount)
+
+    return formattedAmount
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
