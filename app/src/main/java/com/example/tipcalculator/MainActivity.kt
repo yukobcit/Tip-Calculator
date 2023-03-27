a535d6dfb4bc965fa7dbb10b6bdc991541cf5a84
Yuko Ishida <yishida2@my.bcit.ca>
2023-03-27 11:08:06 -0700

    tip calculator version1

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -18,6 +19,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +46,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipCalculatorScreen() {
+    var serviceCostAmountInput by remember { mutableStateOf("") }
+    val amount = serviceCostAmountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -48,19 +59,54 @@ fun TipCalculatorScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.height(16.dp))
-        EditServiceCostField()
+
+
+
+        EditServiceCostField(
+            value = serviceCostAmountInput,
+            onValueChange = { serviceCostAmountInput = it }
+        )
+        Spacer(Modifier.height(24.dp))
+        Text(
+            text = stringResource(R.string.tip_amount, tip),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
+
 @SuppressLint("UnrememberedMutableState")
 @Composable
-fun EditServiceCostField() {
-    var serviceCostAmountInput= mutableStateOf("0")
+fun EditServiceCostField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
     TextField(
-        value = serviceCostAmountInput.value,
-        onValueChange = { serviceCostAmountInput.value = it},
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(stringResource(R.string.service_cost)) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
+
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String {
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Composable
+fun GenerateRandomBill() {
+    val random = (1..100).random()
+    Text(text = "Bill: $random")
+}
+
 
 @Preview(showBackground = true)
 @Composable
